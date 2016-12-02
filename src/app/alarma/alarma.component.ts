6a5c5788f2c166a4c04dc44e9aae2dc7ce0478c8
Jesús Krauer <jesuskrauer@gmail.com>
2016-12-02 17:26:06 -0300

    Agregados control de tiempo, corregido constructor

--- src/app/alarma/alarma.component.ts
+++ src/app/alarma/alarma.component.ts
@@ -5,17 +5,51 @@ import { Component, OnInit } from '@angular/core';
     templateUrl: './alarma.component.html'
 })
 export class AlarmaComponent {
-    horas: number;
-    minutos: number = 24;
-    segundos: number = 59;
-
+    // Se declaran las horas, minutos y segundos a calcular
+    horas: number = 0; minutos: number = 0; segundos: number = 0;
+    // Se declaran las horas, minutos y segundos actuales
+    hActual: number = 0; mActual: number = 0; sActual: number = 0;
+    button: string='Comenzar';
     constructor() {
-        this.horas = 3;
-        this.minutos = 24;
+        this.resetTimer();
+        this.localTime();
+        setInterval(() => this.tick(), 1000)
+    }
+
+    localTime(): void{
+        var d = new Date();
+        this.hActual = d.getHours();
+        this.mActual = d.getMinutes();
+        this.sActual = d.getSeconds();
+    }
+    
+    private resetTimer (): void{
+        // ResetTimer sirve para reiniciar el marcador según figuren los valores abajo
+        this.horas = 23;
+        this.minutos = 59;
         this.segundos = 59;
     }
 
-    ngOnInit (){
+    private tick(): void{
+        // Resta el tiempo, NO TOCAR.
+        if (--this.segundos < 0){
+            this.segundos = 59;
+            if (--this.minutos < 0){
+                this.minutos = 59;
+                if (--this.horas < 0){
+                    this.horas = 23;
+                }
+            }
+        }
+        // Suma el tiempo, NO TOCAR.
+        if (++this.sActual > 59){
+            this.sActual = 0;
+            if (++this.mActual > 59){
+                this.mActual = 0;
+                if (++this.hActual > 23){
+                    this.hActual = 0;
+                }
+            }
+        }
     }
-    
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,36 @@
 import { Component } from '@angular/core';
+import { AlarmaComponent } from './alarma/alarma.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent { }
+export class AppComponent { 
+  hActual: number;
+  mActual: number;
+  sActual: number;
+
+  constructor() {
+    this.localTime();
+    setInterval(() => this.tick(), 1000)
+  }
+  
+  localTime(): void{
+    var d = new Date();
+    this.hActual = d.getHours();
+    this.mActual = d.getMinutes();
+    this.sActual = d.getSeconds();
+  }
+  private tick(): void{
+    if (++this.sActual > 59){
+        this.sActual = 0;
+        if (++this.mActual > 59){
+            this.mActual = 0;
+            if (++this.hActual > 23){
+                this.hActual = 0;
+            }
+        }
+    }
+  }
+}
